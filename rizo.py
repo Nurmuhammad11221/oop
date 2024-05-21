a4c701b526e2adbf36a7b73d0d42fa3fc7853417
Nurmuhammad11221 <roziyaabdurazoqova@gmail.com>
2024-05-21 19:45:22 +0500

    class end

--- rizo.py
+++ rizo.py
@@ -26,29 +26,64 @@
 
 
 class Shaxs:
-    def __init__(self, ism, familya, passport, t_yil, IDraqam):
+    def __init__(self, ism, familya, passport, t_yil):
         self.ism = ism
         self.familya = familya
         self.t_yil = t_yil
         self.passport = passport
-        self.IDraqam = IDraqam
-
+        
     def get_info(self):
         info = f"{self.ism} {self.familya}\n"
-        info += f"{self.passport}, {self.t_yil}-yilda tugilgan, {self.IDraqam}"
+        info += f"{self.passport}, {self.t_yil}-yilda tugilgan"
         return info
-
     def get_age(self, yil):
         age = yil - self.t_yil
         return age
 
-inson = Shaxs("Teshmat", "Boltavoyev", "AC1003243", 2000, "5298822")
-print(inson.get_info())
-print(f"{inson.get_age(2024)} yoshida")
-print(inson.IDraqam)
+inson = Shaxs("Teshmat", "Boltavoyev", "AC1003243", 2000)
+# print(inson.get_info())
+# print(f"{inson.get_age(2024)} yoshida")
 
 
 class Talaba(Shaxs):
     """ Talaba klasi """
-    def __init__(self, ism, familya, passport, t_yil, IDraqam):
+    def __init__(self, ism, familya, passport, t_yil, IDraqam, manzil):
+        super().__init__(ism, familya, passport, t_yil)
+        self.IDraqam = IDraqam
+        self.bosqich = 1
+        self.manzil = manzil
+            
+
+    def get_id(self):
+        """ Talabning id raqam """
+        return self.IDraqam
+    def get_bosqich(self):
+        """ Talabning nechinchi kursligi """
+        return self.bosqich
+    def get_info(self):
+        info = f"{self.ism} {self.familya}\n"
+        info += f"{self.get_bosqich()}- bosqich talabasi, talaba id: #{self.get_id()}"
+        return info
+
+# talaba = Talaba("BOltavoyev", "teshaboy", "AC1003243", 2020, "5198733")
+
+# print(talaba.get_info())
+# print(talaba.get_id())
+
+class Manzil:
+    def __init__(self, uy, kocha, tuman, shaxar):
+        self.uy = uy
+        self.kocha = kocha
+        self.tuman = tuman
+        self.shaxar = shaxar
         
+    def get_manzil(self):
+        """ Manzilni korish """
+        manzil = f"{self.shaxar}, {self.tuman} tumani,\n"
+        manzil += f"{self.kocha}, {self.uy}-uy"
+        return manzil
+        
+talaba_manzil = Manzil(28, "nur", "uchtepa", "tosheknt")
+talaba = Talaba("BOltavoyev", "teshaboy", "AC1003243", 2020, "5198733", talaba_manzil)
+print(talaba_manzil.get_manzil())
+print(talaba_manzil.tuman)
